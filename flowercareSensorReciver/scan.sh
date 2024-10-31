bad70130a39c66423117cbe7db16865e2eef47c0
junghuicheol <dyujhc@gmail.com>
2024-10-31 20:40:38 +0900

    add scan.sh

---
+++ flowercareSensorReciver/scan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 현재 사용자 크론탭 내용 삭제
+crontab -r
+echo "Crontab contents have been deleted."
+
+sudo python sensor_scan.py
+
+# 새로운 크론탭 내용 추가
+echo "* * * * * python /home/pi/WATERs/src/sensorData.py" | crontab -
+echo "New crontab entry added: '* * * * * python /home/pi/WATERs/src/sensorData.py'"
