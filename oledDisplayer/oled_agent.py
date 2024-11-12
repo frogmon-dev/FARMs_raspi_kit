f9b91d6d8b2fcf604289a97908bbd486aded66a7
junghuicheol <dyujhc@gmail.com>
2024-11-12 19:22:58 +0900

    add new test

---
+++ oledDisplayer/oled_agent.py
@@ -0,0 +1,20 @@
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+import time
+
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial, rotate=0)
+
+# Box and text rendered in portrait mode
+with canvas(device) as draw:
+    padding = 2
+    draw.rectangle(
+        (padding, padding, device.width - padding, device.height - padding),
+        outline="white",
+        fill="black"
+    )
+    draw.text((20, 20), "Hello World!", fill="white")
+while 1:
+    time.sleep(1)
+    
