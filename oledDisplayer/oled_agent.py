f251c1f49697ae7309bf33d3b72a7fd35bff1751
junghuicheol <dyujhc@gmail.com>
2024-11-12 19:51:42 +0900

    add new test

--- oledDisplayer/oled_agent.py
+++ oledDisplayer/oled_agent.py
@@ -7,11 +7,11 @@ import time
 # I2C 설정
 serial = i2c(port=1, address=0x3C)
 # 디스플레이를 세로 모드로 설정 (rotate=1 또는 3 사용)
-device = ssd1306(serial, rotate=1)
+device = ssd1306(serial, rotate=0)
 
 # 글꼴 설정 (크기를 키우기 위해 truetype 폰트 로드)
 font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"  # 경로에 적합한 글꼴 사용
-font_size = 14  # 원하는 글씨 크기로 설정
+font_size = 10  # 원하는 글씨 크기로 설정
 font = ImageFont.truetype(font_path, font_size)
 
 # 박스와 텍스트를 세로 모드에 맞게 렌더링
