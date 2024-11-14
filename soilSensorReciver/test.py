504ae66dbe9ac57472de9ccf5bd23d0686005b02
junghuicheol <dyujhc@gmail.com>
2024-11-14 23:18:31 +0900

    change protocols

--- soilSensorReciver/test.py
+++ soilSensorReciver/test.py
@@ -1,88 +1,88 @@
 import time
-from pymodbus.client import ModbusSerialClient  # 최신 구조에 맞게 수정
+import json
+from pymodbus.client import ModbusSerialClient
+from datetime import datetime
+from frogmon.uGlobal import GLOB
 
-# Modbus Serial Client 설정
-modbus_port = '/dev/ttyAMA0'  # 라즈베리 파이의 UART 포트
-client = ModbusSerialClient(port=modbus_port, baudrate=4800, timeout=1)
+# Modbus 설정
+modbus_port = '/dev/ttyAMA0'  # 라즈베리 파이에 연결된 Modbus 포트
+client = ModbusSerialClient(port=modbus_port, baudrate=9600, timeout=1)
 
-# Modbus 명령어 설정 (아두이노 코드에서 사용한 명령어와 동일하게 설정)
-commands = {
-    'temperature': [0x01, 0x03, 0x00, 0x13, 0x00, 0x01, 0x75, 0xcf],
-    'moisture': [0x01, 0x03, 0x00, 0x12, 0x00, 0x01, 0x24, 0x0F],
-    'conductivity': [0x01, 0x03, 0x00, 0x15, 0x00, 0x01, 0x95, 0xce],
-    'ph': [0x01, 0x03, 0x00, 0x06, 0x00, 0x01, 0x64, 0x0b],
-    'nitrogen': [0x01, 0x03, 0x00, 0x1E, 0x00, 0x01, 0xE4, 0x0C],
-    'phosphorus': [0x01, 0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc],
-    'potassium': [0x01, 0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0],
-}
+# 현재 날짜와 CSV 파일명 설정
+currentDate = datetime.now().strftime("%Y%m%d")
+csvFileName = '/home/pi/FARMs_raspi_kit/csv/soildata_'
+setupFileName = '/home/pi/FARMs_raspi_kit/bin/setup.ini'
 
-def send_modbus_command(command):
-    # Modbus 명령을 전송
-    client.write_registers(0, command)
+# 설정 파일에서 MQTT 정보 읽기
+userId = GLOB.get_ini_value(setupFileName, 'SETUP', 'user_id', 'test')
+deviceId = GLOB.get_ini_value(setupFileName, 'AGENT', 'id', 'test')
 
-    # 응답 읽기
-    response = client.read_holding_registers(0, 7)  # 예상 응답 길이 = 7 바이트
-    if response.isError():
-        print("센서 데이터 읽기 실패")
-        return None
+mqttUrl = GLOB.get_ini_value(setupFileName, 'MQTT', 'url', 'frogmon.synology.me')
+mqttPort = int(GLOB.get_ini_value(setupFileName, 'MQTT', 'port', '8359'))
+pubTopic = GLOB.get_ini_value(setupFileName, 'MQTT', 'pub_topic', 'test') + userId + '/' + deviceId
+
+# 데이터 요청 함수 (첫 번째 코드의 구조 적용)
+def get_sensor_data():
+    if client.connect():
+        # Modbus 데이터를 요청하여 7개의 레지스터 값 읽기
+        response = client.read_holding_registers(0x0000, 7, unit=1)
+        
+        if not response.isError():
+            # 읽은 데이터를 배열에 저장
+            data = [
+                response.getRegister(0),  # Humidity
+                response.getRegister(1),  # Temperature
+                response.getRegister(2),  # Conductivity
+                response.getRegister(3),  # pH
+                response.getRegister(4),  # Nitrogen
+                response.getRegister(5),  # Phosphorus
+                response.getRegister(6)   # Potassium
+            ]
+            return data
+        else:
+            print("센서 데이터 읽기 실패")
+            return None
     else:
-        return response.registers
+        print("Modbus 클라이언트 연결 실패")
+        return None
 
-def parse_response(response):
-    # 유효한 응답인지 확인
-    if response and len(response) >= 4:
-        return response[3] << 8 | response[4]  # 데이터 바이트를 합쳐서 반환
-    return None
+# 센서 데이터 해석 (첫 번째 코드의 파싱 방식 적용)
+def parse_sensor_data(data):
+    if data:
+        sensor_data = {
+            "humidity": data[0] * 0.1,
+            "temperature": data[1] * 0.1,
+            "conductivity": data[2],
+            "PH": data[3] * 0.1,
+            "nitrogen": data[4],
+            "phosphorus": data[5],
+            "potassium": data[6]
+        }
+        return sensor_data
+    else:
+        return None
 
-def read_sensor_data():
-    data = {}
-
-    # 수분도
-    response = send_modbus_command(commands['moisture'])
-    data['Moisture'] = parse_response(response) / 10.0 if response else None
-    time.sleep(1)
-
-    # 온도
-    response = send_modbus_command(commands['temperature'])
-    data['Temperature'] = parse_response(response) / 10.0 if response else None
-    time.sleep(1)
-
-    # 전도도
-    response = send_modbus_command(commands['conductivity'])
-    data['Conductivity'] = parse_response(response) if response else None
-    time.sleep(1)
-
-    # pH
-    response = send_modbus_command(commands['ph'])
-    data['pH'] = parse_response(response) / 10.0 if response else None
-    time.sleep(1)
-
-    # 질소
-    response = send_modbus_command(commands['nitrogen'])
-    data['Nitrogen'] = parse_response(response) if response else None
-    time.sleep(1)
-
-    # 인
-    response = send_modbus_command(commands['phosphorus'])
-    data['Phosphorus'] = parse_response(response) if response else None
-    time.sleep(1)
-
-    # 칼륨
-    response = send_modbus_command(commands['potassium'])
-    data['Potassium'] = parse_response(response) if response else None
-    time.sleep(1)
-
-    return data
+# 데이터 출력 및 저장 루프
+def print_loop():
+    while True:
+        currentDate = datetime.now().strftime("%Y%m%d")
+        raw_data = get_sensor_data()
+        
+        if raw_data:
+            sensor_data = parse_sensor_data(raw_data)
+            if sensor_data:
+                print("센서 데이터:")
+                print(sensor_data)
+                
+                # CSV에 데이터 저장
+                GLOB.save_sensor_data_to_csv(csvFileName + currentDate + '.csv', sensor_data)
+                print("센서 데이터가 저장되었습니다.")
+                
+                # MQTT 서버로 데이터 전송
+                GLOB.mqtt_publish(mqttUrl, mqttPort, pubTopic, sensor_data)
+                print("센서 데이터를 서버로 전송")
+                
+        time.sleep(10)
 
 if __name__ == '__main__':
-    try:
-        client.connect()
-        while True:
-            sensor_data = read_sensor_data()
-            if sensor_data:
-                print("센서 데이터:", sensor_data)
-            time.sleep(10)
-    except KeyboardInterrupt:
-        print("프로그램 종료")
-    finally:
-        client.close()
+    print_loop()
